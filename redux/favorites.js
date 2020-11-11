0d7b68800be794f9801f70cb1f974e20129c272c
Edgar Sousa <edgarsousa272@gmail.com>
2020-11-11 06:30:33 -0500

    Alert

--- redux/favorites.js
+++ redux/favorites.js
@@ -9,6 +9,11 @@ export const favorites = (state = [], action) => {
             //concat make copys of an array, adds new item on it, and turns the new array without mutading the previous array.
             return state.concat(action.payload);
 
+        case ActionTypes.DELETE_FAVORITE:
+                /* creating a new array from favorite state by filtering into it every campsite that does not macth the
+                camsite id in the action payload */
+                return state.filter(favorite => favorite !== action.payload);
+
         default:
             return state;
     }
